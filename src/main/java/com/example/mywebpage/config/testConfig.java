dd16db9b90f2db82eb519b56a6178768bde97928
Ostrich5yw <1596253661@qq.com>
2020-10-08 13:38:31 +0800

    mywebpage

--- src/main/java/com/example/mywebpage/config/testConfig.java
+++ src/main/java/com/example/mywebpage/config/testConfig.java
@@ -4,9 +4,9 @@ import com.example.mywebpage.bean.UserBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration      //声明当前类是一个配置类
 public class testConfig {
-    @Bean(name = "test")
+    @Bean(name = "test")       //构建一个实例，放入Spring容器中
     public UserBean setUser(){
         UserBean userBean = new UserBean();
         userBean.setUser_id(1);
